196dd64eba57f18c36fde081a8b8ad727181f977
slazturk <106006892+slazturk@users.noreply.github.com>
2023-09-30 05:08:22 +0300

    Add files via upload

---
+++ StajProjem/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace StajProjem
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "KOÇ",
+            "BOĞA",
+            "İKİZLER",
+            "YENGEÇ",
+            "ASLAN",
+            "BAŞAK",
+            "TERAZİ",
+            "AKREP",
+            "YAY",
+            "OĞLAK",
+            "KOVA",
+            "BALIK"});
+            this.comboBox1.Location = new System.Drawing.Point(509, 42);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(210, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Lucida Bright", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Location = new System.Drawing.Point(231, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Burcunuzu Seçiniz:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button1.Font = new System.Drawing.Font("Lucida Bright", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button1.Location = new System.Drawing.Point(797, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "GÖSTER";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Lucida Bright", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label2.Location = new System.Drawing.Point(231, 459);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 26);
+            this.label2.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(509, 77);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Lucida Bright", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label3.Location = new System.Drawing.Point(415, 459);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 20);
+            this.label3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Lucida Bright", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label4.Location = new System.Drawing.Point(415, 522);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 20);
+            this.label4.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::StajProjem.Properties.Resources.astroloji_nasil_ogrenilir;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1411, 775);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ StajProjem/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StajProjem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int secim=comboBox1.SelectedIndex;
+            switch (secim)
+            {
+                case 0:
+                    label2.Text = " Koç Burcu:";
+                    label3.Text = "Olumlu özellikleri: Enerjik, cesaret verici, durdurulamaz, cesur, adanmış, kahraman, şefkatli";
+                    label4.Text= "Olumsuz özellikleri: Kibirli, benmerkezci, itkisel, buyurgan, inatçı, umursamaz, kıskanç";
+                    pictureBox1.ImageLocation = "astrolojikoc.jpg";
+                    pictureBox1.SizeMode=PictureBoxSizeMode.AutoSize;
+                    break;
+               
+                case 1:
+                    label2.Text = " Boğa Burcu:";
+                    label3.Text = "Olumlu özellikleri: Sabırlı, düzenli, yardımcı, romantik, özenli, adanmış";
+                    label4.Text = "Olumsuz özellikleri: Fazla hoşgörülü, inatçı, tembel, fazla ihtiyatlı";
+                    pictureBox1.ImageLocation = "astrolojiboga.jpg";
+                    pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+                    break;
+               
+                case 2:
+                    label2.Text = " İkizler Burcu:";
+                    label3.Text = "Olumlu özellikleri: Etkileyici, özgün, iş bilen, alımlı, akıllı, maceracı";
+                    label4.Text = "Olumsuz özellikleri: Vesveseli, dengesiz, iki yüzlü, yargılayıcı, depresif";
+                    pictureBox1.ImageLocation = "astrolojiikizler.jpg";
+                    pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+                    break;
+
+                case 3:
+                    label2.Text = " Yengeç Burcu:";
+                    label3.Text = "Olumlu özellikleri: Yardımsever, sabırlı, merhametli, anaç, romantik, yaratıcı";
+                    label4.Text = "Olumsuz özellikleri: Dedikoducu, burnu havada, ketum, aşırı duyarlı, fazla rekabetçi";
+                    pictureBox1.ImageLocation = "astrolojiyengec.jpg";
+                    pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+                    break;
+
+                case 4:
+                    label2.Text = " Aslan Burcu:";
+                    label3.Text = "Olumlu özellikleri: Cesur, kibar, cömert, sadık, koruyucu, dobra, eğlenceli";
+                    label4.Text = "Olumsuz özellikleri: Küstah, müsrif, özensiz, taş kalpli, kıskanç, saldırgan";
+                    pictureBox1.ImageLocation = "astrolojiaslan.jpg";
+                    pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+                    break;
+
+                case 5:
+                    label2.Text = " Başak Burcu:";
+                    label3.Text = "Olumlu özellikleri: Adanmış, becerikli, yardımsever, çalışkan, hazırcevap, pratik";
+                    label4.Text = "Olumsuz özellikleri: Vaaz verme meraklısı, kendi kendine zarar verme, kendine acıma, şekilci, eleştirel";
+                    pictureBox1.ImageLocation = "astrolojibasak.jpg";
+                    pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+                    break;
+
+                case 6:
+                    label2.Text = " Terazi Burcu:";
+                    label3.Text = "Olumlu özellikleri: Çekici, sevimli, adil, cana yakın, paylaşımcı, romantik";
+                    label4.Text = "Olumsuz özellikleri: Faydasız, kararsız, aşırı duygusal, manipülatif, şımarık, kuruntulu";
+                    pictureBox1.ImageLocation = "astrolojiterazi.jpg";
+                    pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+                    break;
+
+                case 7:
+                    label2.Text = " Akrep Burcu:";
+                    label3.Text = "Olumlu özellikleri: Sabırlı, derinlemesine bilgi sahibi, disiplinli, dikkatli";
+                    label4.Text = "Olumsuz özellikleri: Kindar, asıl niyetini gizleyen, herkese karşı güvensiz";
+                    pictureBox1.ImageLocation = "astrolojiakrep.jpg";
+                    pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+                    break;
+
+                case 8:
+                    label2.Text = " Yay Burcu:";
+                    label3.Text = "Olumlu özellikleri: Dürüst, tarafsız, iyimser, coşkulu, cesaretlendirici, adanmış";
+                    label4.Text = "Olumsuz özellikleri: Tartışmacı, umursamaz, tuhaf, vaaz verme meraklısı, nezaketsiz, kendinden çok emin";
+                    pictureBox1.ImageLocation = "astrolojiyay.jpg";
+                    pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+                    break;
+
+                case 9:
+                    label2.Text = " Oğlak Burcu:";
+                    label3.Text = "Olumlu özellikleri: Sadık, ailesine düşkün, çalışkan, adanmış, dürüst, korkusuz";
+                    label4.Text = "Olumsuz özellikleri: Kötümser, bağışlamaz, soğuk, maddiyatçı, burnu havada, umutsuz";
+                    pictureBox1.ImageLocation = "astrolojioglak.jpg";
+                    pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+                    break;
+
+                case 10:
+                    label2.Text = " Kova Burcu:";
+                    label3.Text = "Olumlu özellikleri: Bağımsız, özgün, insani, açık fikirli, adil, mantıklı";
+                    label4.Text = "Olumsuz özellikleri: Tavizsiz, gösterişçi, söz tutmama";
+                    pictureBox1.ImageLocation = "astrolojikova.jpg";
+                    pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+                    break;
+
+                case 11:
+                    label2.Text = " Balık Burcu:";
+                    label3.Text = "Olumlu özellikleri: Romantik, yardımsever, bilge, rahatlatıcı, yaratıcı";
+                    label4.Text = "Olumsuz özellikleri: Saflık, kendine acıma, gerçeklikten kopukluk, kendine zarar verme, bağımlı";
+                    pictureBox1.ImageLocation = "astrolojibalk.jpg";
+                    pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+                    break;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
